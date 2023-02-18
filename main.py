d7ce92914b484782903a88d9e01b6f49d1bc9a4d
I.Muratov <ivanmuratov.2016@gmail.com>
2023-02-18 17:42:07 +0200

    changing function 'parse' name

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-def parse(query: str) -> dict:
+def parsed(query: str) -> dict:
     return {}
 
 
 if __name__ == '__main__':
-    assert parse('https://example.com/path/to/page?name=ferret&color=purple') == {'name': 'ferret', 'color': 'purple'}
-    assert parse('https://example.com/path/to/page?name=ferret&color=purple&') == {'name': 'ferret', 'color': 'purple'}
-    assert parse('http://example.com/') == {}
-    assert parse('http://example.com/?') == {}
-    assert parse('http://example.com/?name=Dima') == {'name': 'Dima'}
+    assert parsed('https://example.com/path/to/page?name=ferret&color=purple') == {'name': 'ferret', 'color': 'purple'}
+    assert parsed('https://example.com/path/to/page?name=ferret&color=purple&') == {'name': 'ferret', 'color': 'purple'}
+    assert parsed('http://example.com/') == {}
+    assert parsed('http://example.com/?') == {}
+    assert parsed('http://example.com/?name=Dima') == {'name': 'Dima'}
 
 
 def parse_cookie(query: str) -> dict:
